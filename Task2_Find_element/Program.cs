0cc9a64da9c9af29e7ccfdc88c1ec2c9cf85f942
«Artur» <art13471118@gmail.com>
2022-12-03 23:03:11 +0300

    Нашел элемент массива, равный find. Но не получатеся вывести команду < Индекса с таким элементом нет>

---
+++ Task2_Find_element/Program.cs
@@ -0,0 +1,23 @@
+﻿// Имеется одномерный массив array из n элементов. Требуется найти элемент массива, равный find.
+
+int[] array = {23, 4, 6, 146, 3058, 6, 4983, 343, 398};
+int n = array.Length;
+int index = 0;
+int find = 6;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+        
+    }
+    index++;
+      
+    
+    
+    
+}
+
+Console.WriteLine("Индеса с таким элементом нет");
